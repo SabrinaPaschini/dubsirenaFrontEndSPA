6ad0f40474cbbae206a2f7e00ab8c53a955618c1
Sabrina Paschini <sabrina.paschini@hotmail.com>
2024-05-20 13:17:06 -0300

    add routes

--- dubSirenaFront/src/app/app-routing.module.ts
+++ dubSirenaFront/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuTitleComponent } from './menu/menu-title/menu-title.component';
+
 import { EquipamentsComponent } from './content/equipaments/equipaments.component';
 
 const routes: Routes = [
-  {path:'', component: MenuTitleComponent, pathMatch:'full'}, // path é caminho, e virgula colocar o componete que quer renderizar 
+  
   {path:'equipo', component: EquipamentsComponent, pathMatch:'prefix'}
 
 ];
